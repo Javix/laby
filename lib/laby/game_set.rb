08dbd6814d5e3e9d9ad1a4df646b9b37ba1f88fb
Javix <s.cambour@gmail.com>
2014-01-27 17:26:53 +0100

    Finish step 2
    - create laby.txt content
    - create game_set methods
    - add .gitignore file for future ignore entries

--- lib/laby/game_set.rb
+++ lib/laby/game_set.rb
@@ -1,28 +1,36 @@
 module Laby
   class GameSet
-    def initialize(file_name)
+    def initialize(file_name = 'laby.txt')
       @file_name = file_name
+      @matrix = {}
     end
 
     def to_s
-      File.open("laby.txt") do |file|
-        #file.each_line {|line| puts "Got #{line.chomp}" }
+      File.open(@file_name) do |file|
         file.each_line do |line|
-          line.each_char { |c| puts c }
+          puts line
+          line.chars.each_with_index do |char, index|
+            @matrix[[file.lineno-1, index]] = Cell.new(char)
+          end
         end
       end
     end
 
     def is_accessible?(x, y)
-
+      cell = @matrix[x, y]
+      cell.type != Cell::TYPES[:wall]
     end
 
     def get_start_coordinates
-      [x, y]
+      start = @matrix.select { |k, v| v.type.eql?(Cell::TYPES[:start])}.first
+      raise "Start case not found" unless start
+      start
     end
 
     def get_finish_coordinates
-
+      finish = @matrix.select { |k, v| v.type.eql?(Cell::TYPES[:finish])}.first
+      raise "Finish case not found" unless finish
+      finish
     end
 
   end
