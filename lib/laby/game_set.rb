76a53b92ee305204245f14194d7025a68a2b4aa8
Javix <s.cambour@gmail.com>
2014-01-28 12:38:58 +0100

    Extract matrix creation into private method

--- lib/laby/game_set.rb
+++ lib/laby/game_set.rb
@@ -2,17 +2,12 @@ module Laby
   class GameSet
     def initialize(file_name = 'laby.txt')
       @file_name = file_name
-      @matrix = {}
+      init_matrix(@file_name)
     end
 
     def to_s
       File.open(@file_name) do |file|
-        file.each_line do |line|
-          puts line
-          line.chars.each_with_index do |char, index|
-            @matrix[[file.lineno-1, index]] = Cell.new(char)
-          end
-        end
+        file.each_line { |line| puts line}        
       end
     end
 
@@ -31,6 +26,19 @@ module Laby
       finish = @matrix.select { |k, v| v.type.eql?(Cell::TYPES[:finish])}.first
       raise "Finish case not found" unless finish
       finish
+    end
+    
+    private
+    def init_matrix(file_name)
+      @matrix = {}
+      File.open(file_name) do |file|
+        file.each_line do |line|          
+          line.chars.each_with_index do |char, index|
+            @matrix[[file.lineno-1, index]] = Cell.new(char)
+          end
+        end
+      end
+      @matrix
     end
 
   end
