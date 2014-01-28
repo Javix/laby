fcd0be3e26608df5081bb78c7f8b826b511161d1
Javix <s.cambour@gmail.com>
2014-01-28 10:01:06 +0100

    Use 'rand' to random select a direction

--- lib/laby/rand_player.rb
+++ lib/laby/rand_player.rb
@@ -1,7 +1,10 @@
 module Laby
   class RandPlayer < Player
     def query_next_direction(game_set)
-      [:left, :up, :right, :down].sample
+      # Commented as the below version using 'rand' is desireed by client
+      #[:left, :up, :right, :down].sample
+      directions = [:left, :up, :right, :down]
+      directions[rand(directions.length)]      
     end
   end
 end
