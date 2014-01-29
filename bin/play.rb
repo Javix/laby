3abf552fab828ae09273fbdf885029e5dda2e828
Javix <s.cambour@gmail.com>
2014-01-29 10:21:29 +0100

    Call Engine#run

--- bin/play.rb
+++ bin/play.rb
@@ -4,37 +4,13 @@ require 'laby'
 
 file_name = ARGV.first || ''
 raise "Labyrinth file is not found!" unless File.exists?(file_name)
-laby = Laby::GameSet.new(file_name)
+game_set = Laby::GameSet.new(file_name)
 puts "Welcome to the Labyrinth:"
 
-def create_players()
-  players = []
-  players << create_player('toto')
-	players << create_player('yoyo')      
-  players
-end
+players = Laby::Support.create_players
+Laby::Support.set_on_start(players, game_set.get_start_coordinates)
 
-def create_player(name)
-	player = Laby::HumanPlayer.new(name)  
-  player
-end
 
-def set_on_start(players, start_coordinates)
-  players.each do |player|
-    player.x = x_start(start_coordinates)
-    player.y = y_start(start_coordinates)
-  end
-end
 
-def x_start(coords)
-	coords.first
-end
-
-def y_start(coords)
-  coords.last
-end
-
-players = create_players
-set_on_start(players, laby.get_start_coordinates)
-
-laby.to_s(players)
+engine = Laby::Engine.new(game_set, players)
+engine.run
