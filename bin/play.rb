b5ef5db680862968aab87d081aa0f88925c52f5e
Javix <s.cambour@gmail.com>
2014-01-30 09:27:46 +0100

    Assigns an empty string if no file provided in ARGV

--- bin/play.rb
+++ bin/play.rb
@@ -1,8 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'laby'
 
-file_name = ARGV.pop
-raise "Labyrinth file is not found!" unless File.exists?(file_name)
+file_name = ARGV.pop || ''
+raise "Labyrinth file #{file_name}is not found!" unless File.exists?(file_name)
 game_set = Laby::GameSet.new(file_name)
 puts "Welcome to the Labyrinth"
 
