e0b6bee393efb1ad4be5a4e5e1c3ecb65e7f898f
Serguei Cambour <s.cambour@gmail.com>
2021-05-08 12:02:43 +0200

    Fix rubocop warnings,re-run `bundle`

--- bin/play.rb
+++ bin/play.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'laby'
 
@@ -15,9 +17,12 @@ require 'laby'
 # And displays the created maze with a greeting for the first player
 
 file_name = ARGV.pop || ''
-raise "Labyrinth file #{file_name} is not found! \n You can use 'laby.txt' as example available in the code source" unless File.exists?(file_name)
+unless File.exist?(file_name)
+  raise "Labyrinth file #{file_name} is not found! \n You can use 'laby.txt' as example available in the code source"
+end
+
 game_set = Laby::GameSet.new(file_name)
-puts "Welcome to the Labyrinth"
+puts 'Welcome to the Labyrinth'
 
 players = Laby::Support.create_players
 Laby::Support.set_on_start(players, game_set.get_start_coordinates)
